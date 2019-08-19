187686db572973fb74adc7fbb231af8419dafbc6
Ray Guillotte <rayguillotte@Rays-Air.fios-router.home>
2019-08-19 19:36:28 -0400

    created AppRouter and TabNav to Link pages together

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import AppRouter from "./components/AppRouter.js";
 
-import { Route } from "react-router-dom";
-
-import CharacterList from "./components/CharacterList";
-import LocationsList from "./components/LocationsList";
-import EpisodesList from "./components/EpisodesList";
-
 export default function App() {
   return (
     <main>
-      {/* <Route path="/characters" component={CharacterList} />
-      <Route path="/locations" component={LocationsList} />
-      <Route path="/episodes" component={EpisodesList} /> */}
       <Header />
       <TabNav />
-      {/* <AppRouter /> */}
+      <AppRouter />
     </main>
   );
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Tab, Menu, Icon } from "semantic-ui-react";
+import { Menu } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 // TODO: Add missing tabs below
-export default function TabNav() {
+export default function TabNav({}) {
+
+    return (
+        <Menu tabular>
+          <Menu.Item name='home'><NavLink to="/">Home</NavLink></Menu.Item>
+          <Menu.Item name='characters'><NavLink to="/characters">Characters</NavLink></Menu.Item>
+          <Menu.Item name='locations'><NavLink to="/locations">Locations</NavLink></Menu.Item>
+          <Menu.Item name='episodes'><NavLink to="/episodes">Episodes</NavLink></Menu.Item>
+        </Menu>
+      )
 
 };
