8f8787ad56f3ea6ce60f9d142da56ddf35086cf4
Ray Guillotte <rayguillotte@Rays-Air.fios-router.home>
2019-08-19 00:01:31 -0400

    started Nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import AppRouter from "./components/AppRouter.js";
 
+import { Route } from "react-router-dom";
+
 import CharacterList from "./components/CharacterList";
 import LocationsList from "./components/LocationsList";
 import EpisodesList from "./components/EpisodesList";
@@ -10,12 +12,12 @@ import EpisodesList from "./components/EpisodesList";
 export default function App() {
   return (
     <main>
-      <CharacterList />
-      <LocationsList />
-      <EpisodesList />
-      {/* <Header />
+      {/* <Route path="/characters" component={CharacterList} />
+      <Route path="/locations" component={LocationsList} />
+      <Route path="/episodes" component={EpisodesList} /> */}
+      <Header />
       <TabNav />
-      <AppRouter /> */}
+      {/* <AppRouter /> */}
     </main>
   );
 }
